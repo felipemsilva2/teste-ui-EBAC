9197c2428d5aea6da0fcd4e30bf88af12251f42f
Felipe Magalhães <98434957+felipemsilva2@users.noreply.github.com>
2023-03-11 16:07:35 -0300

    usando massa de dados

--- cypress/e2e/login-ebac-ui.cy.js
+++ cypress/e2e/login-ebac-ui.cy.js
@@ -1,24 +1,41 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
-context('Funcionalidade Login' , () =>{
+context('Funcionalidade Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
         cy.screenshot()
     });
 
-    it('Deve fazer login com sucesso' , () => {
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('a > .hidden-xs').should('contain' , 'Welcome')
-    
+        cy.get('a > .hidden-xs').should('contain', 'Welcome')
+
+        it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+            cy.get('#username').type(perfil.usuario)
+            cy.get('#password').type(perfil.senha)
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('a > .hidden-xs').should('contain', 'Welcome')
+        })
+
+        it('Deve fazer login com sucesso usando fixture', () => {
+            cy.fixture('perfil.json').then(dados => {
+                cy.get('#username').type(dados.usuario)
+                cy.get('#password').type(dados.senha, {log: false})
+                cy.get('.woocommerce-form > .button').click()
+                cy.get('a > .hidden-xs').should('contain', 'Welcome')
+            })
+        })
+
     })
 
-    it('Deve exibir uma mensagem de erro ao inserir usuário inválido' , () => {
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -26,7 +43,7 @@ context('Funcionalidade Login' , () =>{
 
     })
 
-    it('Deve exibir uma mensagem de erro ao inserir senha inválida' , () => {
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
