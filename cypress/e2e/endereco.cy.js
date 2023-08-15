17e44bf6ad1414f96cce9db3e939186908dadb1e
Felipe Magalhães <felipemsilva93@gmail.com>
2023-08-15 20:38:56 -0300

    usando massa de dados em lista

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import enderecoPage from "../support/page-objects/endereco.page";
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -9,8 +10,26 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         })
     })
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
         enderecoPage.editarEnderecoFaturamento('Felipe', 'Magalhães', 'EBAC', 'Brasil', 'CLN 204 NORTE', '204', 'Brasília', 'Distrito Federal', '70842-530', '61-99956-1191', 'felipe@gmail.com')
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
+    
+    });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivos de dados', () => {
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].país,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+        )
         cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
     
     });
