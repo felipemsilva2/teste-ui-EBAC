080df3d6f2468c552b0959442d413f67d113c518
Felipe Magalhães <felipemsilva93@gmail.com>
2023-08-15 21:15:42 -0300

    exercicio testes end-to-end plataforma ebac

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     "baseUrl": "http://lojaebac.ebaconline.art.br",
+    "projectId": "aaii8w",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
